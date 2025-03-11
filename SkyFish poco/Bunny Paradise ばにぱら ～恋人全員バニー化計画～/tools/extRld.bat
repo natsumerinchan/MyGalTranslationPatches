43b206751c2c86f09537b7ada6f2133c76e19bc9
Celica Sylphil <kamikitakomaric@protonmail.com>
2025-03-11 15:16:58 +0800

    Update bat

--- SkyFish poco/Bunny Paradise ばにぱら ～恋人全員バニー化計画～/tools/extRld.bat
+++ SkyFish poco/Bunny Paradise ばにぱら ～恋人全員バニー化計画～/tools/extRld.bat
@@ -1,10 +1,13 @@
 @chcp 65001
 @echo off
 
-rd .\txt /s /q
-md .\txt
+@REM 清理旧内容
+rd .\txt /s /q & md .\txt
+@REM 解密rld文件
 python .\rld_dec.py
+@REM 删除未解密rld文件并重命名*.bin为*.rld
 cd rld && rm *.rld && ren *.bin *.rld
+@REM 提取文本
 cd .. && python .\extRld.py
 echo "[TIPS]请不要重复执行，否则*.rld会被错误地再次解密"
 pause
--- SkyFish poco/Bunny Paradise ばにぱら ～恋人全員バニー化計画～/tools/packRld.bat
+++ SkyFish poco/Bunny Paradise ばにぱら ～恋人全員バニー化計画～/tools/packRld.bat
@@ -1,9 +1,14 @@
 @chcp 65001
 @echo off
 
-rd .\rld_chs /s /q && md .\rld_chs
+@REM 清理旧文件
+rd .\rld_chs /s /q & md .\rld_chs
+@REM 回封文本到rld文件
 python .\packRld.py
+@REM 重命名*.rld为*.bin
 cd rld_chs && ren *.rld *.bin
+@REM 加密rld文件
 cd .. && python .\rld_enc.py
+@REM 删除未加密rld文件
 cd rld_chs && rm *.bin
 pause
